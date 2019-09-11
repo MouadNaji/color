00e52fc0596ec473c43425a253881288dce14eed
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-09-11 23:47:05 +0200

    first

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function selectColor(event) {
   const color = event.target.value;
   console.log(color);
   convertHEXtoRGB(color);
-  document.querySelector(".color").style.backgroundColor = `${color}`;
+  //document.querySelector(".color").style.backgroundColor = `${color}`;
   document.querySelector(".hex1").textContent = `Hex: ${color}`;
 }
 
@@ -15,7 +15,7 @@ function convertHEXtoRGB(color) {
   let g = parseInt(color.substring(3, 5), 16);
   let b = parseInt(color.substring(5, 7), 16);
 
-  document.querySelector(".RGB").textContent = `RGB(${r},${g},${b}`;
+  document.querySelector(".RGB").textContent = `RGB: (${r},${g},${b})`;
   convertRGBtoHSL(r, b, g);
 }
 
@@ -56,9 +56,59 @@ function convertRGBtoHSL(r, g, b) {
 
   console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
   // function convertHEXtoRGB(color) {}{}{}{}
-  h = Math.floor(h * 10);
-  s = Math.floor(s * 10);
-  l = Math.floor(l * 10);
+  h = Math.floor(h * 1);
+  s = Math.floor(s * 1);
+  l = Math.floor(l * 1);
 
   document.querySelector(".HSL").textContent = `HSL: ${h}, ${s}, ${l}`;
+  document.querySelector(
+    ".color"
+  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+
+  document.querySelector(".HSL2").textContent = `HSL: ${h}, ${s}, ${l - 30}`;
+  document.querySelector(
+    ".color2"
+  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l - 30}%)`;
+
+  document.querySelector(".HSL3").textContent = `HSL: ${h}, ${s}, ${l - 20}`;
+  document.querySelector(
+    ".color3"
+  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l - 20}%)`;
+
+  document.querySelector(".HSL4").textContent = `HSL: ${h}, ${s}, ${l + 10}`;
+  document.querySelector(
+    ".color4"
+  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l + 10}%)`;
+
+  document.querySelector(".HSL5").textContent = `HSL: ${h}, ${s}, ${l + 20}`;
+  document.querySelector(
+    ".color5"
+  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l + 20}%)`;
+
+  hslToRgb(h, s, l);
+}
+
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  console.log();
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
